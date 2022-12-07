45bc51572a51151f590e56c1846b3f2af3a6e389
Zak <zak.johansson@gmail.com>
2022-12-06 17:04:55 -0800

    add tweet counter feature

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
-console.log("what?")
+  console.log("works!")
+
+  $("#tweet-text").on("input", function() {
+    let thisContent = $(this).val()
+    $(".counter").val(140 - thisContent.length)
+    if (thisContent.length > 140){
+      $(".counter").css("color", "red");
+    }
+  });
 });
+
